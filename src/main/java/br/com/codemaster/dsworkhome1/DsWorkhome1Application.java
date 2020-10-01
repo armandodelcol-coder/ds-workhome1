bb43d33994c746238dc26991428b143e9ec06f69
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-10-01 11:09:38 -0300

    Create Poject

---
+++ src/main/java/br/com/codemaster/dsworkhome1/DsWorkhome1Application.java
@@ -0,0 +1,13 @@
+package br.com.codemaster.dsworkhome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsWorkhome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsWorkhome1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codemaster/dsworkhome1/DsWorkhome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codemaster.dsworkhome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsWorkhome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
